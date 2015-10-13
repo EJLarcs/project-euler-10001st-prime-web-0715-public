c73800d6fae4009e073cb494ba495dd9715006c1
EJLarcs <ejlarcombe@gmail.com>
2015-10-13 00:35:50 -0400

    Completed

--- lib/10001st_prime.rb
+++ lib/10001st_prime.rb
@@ -1,1 +1,25 @@
 # Implement your procedural solution here!
+def prime_number_for(nth_element)
+  prime_numbers = []
+  n = 2
+  while prime_numbers.length < nth_element
+    if is_prime?(n)
+      prime_numbers << n
+    end
+  n += 1
+  end
+  prime_numbers[-1]
+end
+
+def is_prime?(i)
+   n = 2
+     loop do
+       if n == i
+         return true
+       elsif i % n == 0
+         return false
+       else
+         n += 1
+       end
+     end
+ end
--- lib/oo_10001st_prime.rb
+++ lib/oo_10001st_prime.rb
@@ -1,1 +1,33 @@
 # Implement your object-oriented solution here!
+class Prime
+
+  def initialize(nth_element)
+    @nth_element = nth_element
+  end
+
+  def number
+    prime_numbers = []
+    i = 2
+    while prime_numbers.length < @nth_element
+      if is_prime?(i)
+        prime_numbers << i
+      end
+    i += 1
+    end
+    prime_numbers[-1]
+  end
+
+  def is_prime?(i)
+     n = 2
+       loop do
+         if n == i
+           return true
+         elsif i % n == 0
+           return false
+         else
+           n += 1
+         end
+       end
+   end
+
+end
